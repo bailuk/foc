43dcf90fffe0f987857c163c209b1d06ca66747b
bailuk <bailu@bailu.ch>
2021-12-05 13:30:44 +0100

    Add Factory classes

--- foc-android/src/main/java/ch/bailu/foc_android/FocAndroidFactory.java
+++ foc-android/src/main/java/ch/bailu/foc_android/FocAndroidFactory.java
@@ -1,2 +1,19 @@
-package ch.bailu.foc_android;public class FocAndroidFactory {
+package ch.bailu.foc_android;
+
+import android.content.Context;
+
+import ch.bailu.foc.Foc;
+import ch.bailu.foc.FocFactory;
+
+public class FocAndroidFactory implements FocFactory {
+    private final Context context;
+
+    public FocAndroidFactory(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public Foc toFoc(String string) {
+        return FocAndroid.factory(context, string);
+    }
 }
--- foc-android/src/main/java/ch/bailu/foc_android/FocAssetFactory.java
+++ foc-android/src/main/java/ch/bailu/foc_android/FocAssetFactory.java
@@ -1,2 +1,20 @@
-package ch.bailu.foc_android;public class FocAssetFactory {
+package ch.bailu.foc_android;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import ch.bailu.foc.Foc;
+import ch.bailu.foc.FocFactory;
+
+public class FocAssetFactory implements FocFactory {
+    private final AssetManager assets;
+
+    public FocAssetFactory(Context context) {
+        assets = context.getAssets();
+    }
+
+    @Override
+    public Foc toFoc(String string) {
+        return new FocAsset(assets, string);
+    }
 }
